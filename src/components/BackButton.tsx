00809de7823a03773e9119cd0db46a8d73906fb8
Nav <nav@navrao.me>
2021-07-10 10:23:55 +1200

    feat: tightening the back button

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -6,7 +6,7 @@ const BackButton = (props: any) => (
 		type="button"
 		className="inline-flex items-center px-3 py-2 border border-gray-600 text-sm leading-4 font-medium rounded-md text-gray-600 bg-transparent hover:bg-gray-100 focus:outline-none"
 	>
-		<ArrowLeftIcon className="-ml-0.5 mr-2 h-4 w-4" aria-hidden="true" />
+		<ArrowLeftIcon className="-ml-0.5 mr-1 h-4 w-4" aria-hidden="true" />
 		Back
 	</button>
 )
